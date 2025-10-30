00d61e4e213cf3f3c7d16bacba8579d8a69a9f05
MHD-AKEEL <mhdakeel7755@gmail.com>
2025-10-30 21:54:58 +0300

    modified

---
+++ my_first_project/src/app/components/products/products.ts
@@ -0,0 +1,11 @@
+
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-products',
+  standalone: true,
+  imports: [],
+  template: '<div><h2>This is inline</h2></div>',
+  styleUrls: ['./products.css'],
+})
+export class ProductsComponent {}
